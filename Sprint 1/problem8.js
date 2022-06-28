d3139970e9ca81882c2a5dfa43bd8ecf2074f7a8
Harshvyas2704 <101619449+Harshvyas2704@users.noreply.github.com>
2022-06-28 21:41:56 +0530

    Add files via upload

---
+++ Sprint 1/problem8.js
@@ -0,0 +1,36 @@
+// Product of Array (Recursively) Ended
+// Description
+
+// You are given an array having a particular size N
+
+// You need to find out the product of all the elements of the array
+
+// The only condition being, you need to write a recursive code for it
+
+// The functionproductArraydoes should return the product of the array, but the function should be recursive
+
+
+// Input
+// The first line containsN, the size of the array
+
+// The next line contains the elements stored inarr
+
+// Constrains
+
+// 1 <=N<= 30
+
+// 1 <=arr[i]<= 30
+
+
+// Output
+// Print the product of all the elements of the array, using a recursive function
+
+// Sample Input 1 
+
+// 5
+// 1 2 3 4 5
+// Sample Output 1
+
+// 120
+
+// code
